52998e88cde38b829c09a43ae8020a250db9df76
tom <tom.georgiadis.io@gmail.com>
2022-05-28 16:03:14 +0300

    final commit

--- test_task_project/booking_app/views.py
+++ test_task_project/booking_app/views.py
@@ -13,6 +13,6 @@ class ReservationList(ListView):
         #  filters previous reservations by previous checkin dates, OuterRef references the checkin date of current query object
         #  orders results by descending checkin
         #  slices the pk of the first result, which is the reservation with prior and closest date
-        sub_query = Reservation.objects.filter(rental=OuterRef(    
+        previous_reservation_query = Reservation.objects.filter(rental=OuterRef(    
             "rental"), checkin__lt=OuterRef("checkin")).order_by("-checkin").values("pk")[:1]
-        return Reservation.objects.order_by('rental', 'checkin').annotate(previous_reservation_id=Subquery(sub_query))
+        return Reservation.objects.order_by('rental', 'checkin').annotate(previous_reservation_id=Subquery(previous_reservation_query))
